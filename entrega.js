4a1164fef6dd899efcc735f6feb35c7344e9fe98
Josefina Cardeilhac <cardeilhac.josefina@gmail.com>
2024-04-16 23:48:57 -0300

    1ra ebtrefa

--- entrega.js
+++ entrega.js
@@ -1,37 +1,18 @@
-//ES6
-const expMath = Math.pow(2, 3);
+class ProductManager {
+    constructor(){
+        this.products = [];
+    }
 
-//ES7
-const exponente = 2 ** 3;
-
-console.log(expMath);
-console.log(exponente);
-
-/* ------------------------------------ - ----------------------------------- */
-
-//includes
-
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-console.log("existe dentro del array", array.includes(5));
-console.log("no existe dentro del array", array.includes(11));
-
-const personas = [
-  { nombre: "Juan", edad: 25 },
-  { nombre: "María", edad: 30 },
-  { nombre: "Pedro", edad: 35 },
-];
-
-// const edadBuscada = 30
-// const existePersonaConEdad = personas.some((persona) => {
-//     return persona.edad === edadBuscada
-// })
-
-// console.log(existePersonaConEdad);
-
-// const edadBuscada = 30
-// const existePersonaConEdad = personas.find((persona) => {
-//     return persona.edad === edadBuscada
-// })
-
-// console.log(existePersonaConEdad);
+    addEvent(title, description, price, tumbnail, code, stock()){
+        const event = {
+            id: this.products() + 1,
+            title,
+            description,
+            price,
+            tumbnail,
+            code,
+            stock;
+        };
+        this.products.push(event);
+    }
+}
